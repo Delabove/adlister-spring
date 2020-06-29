aa94622389193cb855a0607d67d41d06c66718b9
DeLayne LaBove <delayne.labove@gmail.com>
2020-06-29 08:55:32 -0500

    Roll Dice Review still working on random number

---
+++ src/main/java/com/codeup/blogreview/controllers/RollDiceController.java
@@ -0,0 +1,36 @@
+package com.codeup.blogreview.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Controller
+public class RollDiceController {
+
+@GetMapping("/roll-dice/{guess}")
+
+public String usersGuess(@PathVariable int guess, Model model){
+
+
+//    random number
+int random = ThreadLocalRandom.current().nextInt(1, 6 + 1);
+
+    model.addAttribute("isCorrectGuess", random == guess);
+    model.addAttribute("guess", guess);
+    model.addAttribute("random", guess);
+    return "roll-dice";
+
+}
+
+
+    @GetMapping("/roll-dice")
+
+    public String showDice(){
+        return "roll-dice";
+    }
+
+
+}
