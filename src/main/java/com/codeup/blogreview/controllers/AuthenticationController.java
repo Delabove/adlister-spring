a279e2f275a450de8bc40b11eed8cd0cc6786b4b
DeLayne LaBove <delayne.labove@gmail.com>
2020-08-09 23:15:12 -0500

    Working on debugging error and redirect

--- src/main/java/com/codeup/blogreview/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/blogreview/controllers/AuthenticationController.java
@@ -1,11 +1,9 @@
 package com.codeup.blogreview.controllers;
 
+import com.codeup.blogreview.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,6 +14,11 @@ public class AuthenticationController {
         return "users/login";
     }
 
+    @PostMapping("/login")
+    public String saveUser(@ModelAttribute User user){
+
+        return "redirect:/posts";
+    }
 
 
 }
